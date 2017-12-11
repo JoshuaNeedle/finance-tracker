451bbedf76a3745f34850513fe4757b33f24a4e0
Joshua Needle <joshua.a.needle@gmail.com>
2017-12-11 00:13:51 +0000

    Add friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search-stocks', to: 'stocks#search'
+  get "my_friends", to: "users#my_friends"
 
   resources :user_stocks, only:[:create, :destroy]
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
